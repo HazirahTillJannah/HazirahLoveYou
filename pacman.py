9099ceca2a51d84af246ee05babf1ce9018574b0
SMKPLMHACKERS <61570604+HazirahTillJannah@users.noreply.github.com>
2020-02-28 11:17:44 +0800

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -1,13 +1,14 @@
 
 echo: HI  (That You know That I love You)
-clear
+
 echo: Kamu mungkin cintakan dia tapi dia mungkin mempunyai orang lain
-clear 
+
 echo:Cinta kamu mungkin bertepuk sebelah tangan 
-clear
+
 echo:Tak mengapa dia mungkin tidak mengenal kamu dgn ckp 
-clear
+
 echo:Tak pa....
-clear
+
 echo:Kita tunggu Satu hari nanti....
-clear
+
+Guna tools aku sebaik mungkin
