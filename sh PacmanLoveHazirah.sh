53c32c1888c7abd3435a46e394342e68a24dbfe4
SMKPLMHACKERS <61570604+HazirahTillJannah@users.noreply.github.com>
2020-02-28 11:05:33 +0800

    PacmanLoveHazirah

---
+++ sh PacmanLoveHazirah.sh
@@ -0,0 +1,15 @@
+echo:username: Pacman
+echo:password: LoveHazirah
+echo:Whatapps:0193919210
+echo:Ig:pitpanzz__
+echo: HI  (That You know That I love You)
+echo: Kamu mungkin cintakan dia tapi dia mungkin mempunyai orang lain
+clear 
+echo:Cinta kamu mungkin bertepuk sebelah tangan 
+clear
+echo:Tak mengapa dia mungkin tidak mengenal kamu dgn ckp 
+clear
+echo:Tak pa....
+clear
+echo:Kita tunggu Satu hari nanti....
+clear
