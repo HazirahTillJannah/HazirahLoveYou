8446ed775a9662689970d42499311913897148b0
SMKPLMHACKERS <61570604+HazirahTillJannah@users.noreply.github.com>
2020-03-06 17:34:18 +0800

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -1,13 +1,5 @@
 
-echo:Assalamualaikum Hazirah Ini Crush Kamu PacmanSmkPlm Hacker
-clear:bash
-show:
-echo:Saya cintakan kamu tapi kamu mungkin mempunyai orang lain
-clear:bash
-show:
-echo:Aku tunggu satu hari nanti untuk menyarungkan cincinku pada mu
-clear:bash
-show:
-echo:Guna tools aku sebaik mungkin
-clear:bash
-echo:Asslamualaikum dari Team GhostXCode7
+Assalamualaikum Hazirah Ini Crush Kamu PacmanSmkPlm Hacker
+Saya cintakan kamu tapi kamu mungkin mempunyai orang laAku tunggu satu hari nanti untuk menyarungkan cincinku pada mu
+Guna tools aku sebaik mungkin
+Asslamualaikum dari Team GhostXCode7
